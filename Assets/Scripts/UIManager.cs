36b44e45336c6cb17ded4170c0772348bf21bc10
Toon Van Hoye <Toonvanhoye@outlook.com>
2023-02-07 23:25:06 +0100

    v0.1.0-aplha Release

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,22 +1,26 @@
 using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] private Button clientBtn;
+    [SerializeField] private Button restartBtn;
+    [SerializeField] private Button exitBtn;
 
     private void Awake()
     {
-        clientBtn.onClick.AddListener(() =>
+        restartBtn.onClick.AddListener(() =>
         {
             Debug.Log("Restart");
             if (NetworkManager.Singleton != null)
             {
                 NetworkManager.Singleton.Shutdown();
             }
+
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         });
+        exitBtn.onClick.AddListener(Application.Quit);
     }
 }
