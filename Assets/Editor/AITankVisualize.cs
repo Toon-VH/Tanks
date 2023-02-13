db9a94165c67350fe5e1838a51c9b9e00a4c19a2
Toon Van Hoye <Toonvanhoye@outlook.com>
2023-02-13 22:58:07 +0100

    Fix Gizmos For scriptableObjects add ExtendedScriptableObjectDrawer.cs

---
+++ Assets/Editor/AITankVisualize.cs
@@ -0,0 +1,52 @@
+﻿using Ai_s;
+using Ai_s.V_1;
+using Tank;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [UnityEditor.CustomEditor(typeof(AIController))]
+    public class AITankVisualize : UnityEditor.Editor
+    {
+        [SerializeField] private AIController _tankController;
+        [SerializeField] private AITankData data;
+
+
+        private void OnEnable()
+        {
+            _tankController = (AIController)target;
+            data = _tankController.data;
+        }
+
+        private void OnSceneGUI()
+        {
+            //FOV
+            var position = _tankController.transform.position;
+            Handles.color = Color.cyan;
+            var viewAngleA = DirFromAngle(-data.ViewAngle / 2, false);
+            var viewAngleB = DirFromAngle(data.ViewAngle / 2, false);
+            Handles.DrawLine(position, position + viewAngleA * data.ViewRadius);
+            Handles.DrawLine(position, position + viewAngleB * data.ViewRadius);
+            var forward1 = _tankController.transform.forward;
+            Handles.DrawWireArc(position, Vector3.up, forward1, data.ViewAngle / 2, data.ViewRadius);
+            Handles.DrawWireArc(position, Vector3.up, forward1, -data.ViewAngle / 2, data.ViewRadius);
+            Handles.color = Color.blue;
+            Handles.DrawWireArc(position, Vector3.up, Vector3.forward, 360, data.AgroRadius);
+            //Angle radius
+            Handles.color = Color.red;
+            Handles.DrawWireArc(position, Vector3.up, forward1, data.TurretRotatingDegree / 2, 1);
+            Handles.DrawWireArc(position, Vector3.up, forward1, -data.TurretRotatingDegree / 2, 1);
+        }
+
+        private Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+        {
+            if (!angleIsGlobal)
+            {
+                angleInDegrees += _tankController.transform.eulerAngles.y;
+            }
+
+            return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+        }
+    }
+}
--- Assets/Scripts/Ai's/V-1/AITankData.cs
+++ Assets/Scripts/Ai's/V-1/AITankData.cs
@@ -11,13 +11,14 @@ namespace Ai_s.V_1
 
         [Header("Field of View")] public float AgroRadius = 15f;
         public float ViewRadius = 50f;
-        public float ViewAngle = 30f;
+        [Range(0, 360)] public float ViewAngle = 30f;
         [Range(0, 360)] public float TurretRotatingDegree = 40f;
         public float MinSecondsBetweenLookingPos = 1f;
         public float MaxSecondsBetweenLookingPos = 3f;
 
         [Header("Accuracy")] [Range(0, 100)] [SerializeField]
         public float Accuracy = 50;
+
         public float MaxAngleMissedShot = 15;
     }
 }
